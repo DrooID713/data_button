0f438937a7c1e038c14ffb1970d421b07ad51494
Марион Бахшиева <landroverx.wow@gmail.com>
2024-09-10 18:34:11 +0300

    frontend

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => {
 
       {isLoading && <h2>Loading...</h2>}
 
-      {data.data.map(person => {
+      {data.result.map(person => {
         return (
           <div key={person._id}>
             <h2>{person.name}</h2>
